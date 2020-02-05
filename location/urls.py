6dfd92e5b9de0ae174520e77ce1f56213c31e910
Rashed <rashed@ishraak.com>
2020-02-05 16:28:07 +0600

    division api update done

--- location/urls.py
+++ location/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 from .api import *
 urlpatterns = [
-    path('division', Division.as_view()),
+    path('division', DivisionListCreate.as_view()),
+    path('division/<str:pk>/', DivisionUpdateDestroy.as_view()),
     path('district', District.as_view()),
+
 ]
