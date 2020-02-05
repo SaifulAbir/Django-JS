8dd7ddb321b353d79f40284b922b93b0ff318a69
Rashed <rashed@ishraak.com>
2020-02-05 16:01:00 +0600

    requirements.txt udpated

--- job/urls.py
+++ job/urls.py
@@ -3,4 +3,7 @@ from django.views.generic import TemplateView
 from job.api import *
 urlpatterns = [
     path('company', CompanyList.as_view()),
+    path('', TemplateView.as_view(template_name='post-job.html')),
+    path('details', TemplateView.as_view(template_name='job-details.html')),
+
 ]
