8c311ae5ad4531355b138a6902dd240588fa6266
Rashed <rashed@ishraak.com>
2020-02-05 12:49:03 +0600

    Company model added....

--- p7/settings_dev.py
+++ p7/settings_dev.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'job'
+    'job',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
