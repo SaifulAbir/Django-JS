38c96d7491419ce3c69612aac88d3efbde34b98c
Rashed <rashed@ishraak.com>
2020-02-05 10:57:06 +0600

    setting folder removed.

--- job/urls.py
+++ job/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from django.views.generic import TemplateView
 urlpatterns = [
-    path('foo/', TemplateView.as_view(template_name='job-details.html'))
+    path('', TemplateView.as_view(template_name='job-details.html'))
 ]
