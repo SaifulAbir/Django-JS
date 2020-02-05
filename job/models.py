c170af84c27b206cad446a602baba0363be3e2a5
Rashed <rashed@ishraak.com>
2020-02-05 11:59:58 +0600

    template added and working on image link

--- job/models.py
+++ job/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    option_text = models.CharField(max_length=1000)
+
+
+
+    def __str__(self):
+        return '%d: %s' % (self.id, self.option_text)
+
+    class Meta:
+        verbose_name_plural = "Questions Options"
--- job/urls.py
+++ job/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='job-details.html'))
+    path('', TemplateView.as_view(template_name='post-job.html')),
+    path('details', TemplateView.as_view(template_name='job-details.html'))
 ]
