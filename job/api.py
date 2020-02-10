c3ac22faf019f5db8dd2f98bcab82adccde040cd
Rashed <rashed@ishraak.com>
2020-02-10 18:46:03 +0600

    working on job list

--- job/api.py
+++ job/api.py
@@ -6,26 +6,6 @@ from .serializers import CompanySerializer, IndustrySerializer,JobTypeSerializer
 from rest_framework.response import Response
 from rest_framework import generics
 
-
-# @api_view(["POST"])
-# def joblist(request):
-#     received_json_data = json.loads(request.body)
-#     email = received_json_data["email"]
-#
-#     data = {
-#         'status': 1,
-#         'code': 200,
-#         "message": 'ok',
-#         "result": {
-#             "user": {
-#                 "email": email
-#             }
-#         }
-#     }
-#
-#     return Response(data)
-
-
 class CompanyList(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
