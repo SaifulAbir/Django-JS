8b11d5d876a3a93a9e8b7756c944be7a8c77603a
shoab <shoab@ishraak.com>
2020-02-10 15:54:49 +0600

    Registered all support model to admin

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from job.models import Company,JobType,Experience,Qualification,Gender,Industry
+
+admin.site.register(Company)
+admin.site.register(JobType)
+admin.site.register(Experience)
+admin.site.register(Qualification)
+admin.site.register(Gender)
+admin.site.register(Industry)
