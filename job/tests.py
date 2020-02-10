2aec6d0b56236950c82d5268cb22472b9a4ff4fc
saiful <saiful@ishraak.com>
2020-02-10 15:18:07 +0600

    Unittest checked and newly added for gender

--- job/tests.py
+++ job/tests.py
@@ -120,6 +120,41 @@ class QualificationTest(TestCase):
 
 #QUALIFICATION TESTS
 
+#GENDER TESTS
+
+class GenderTest(TestCase):
+
+    def test_when_everything_required_is_given_should_pass(self):
+        gender = Gender(name='Male')
+        try:
+            gender.full_clean()
+        except:
+            self.fail()
+
+    def test_when_name_is_null_should_raise_error(self):
+        gender = Gender()
+        with self.assertRaises(ValidationError):
+            gender.full_clean()
+
+    def test_when_name_is_blank_should_raise_error(self):
+        gender = Gender(name='')
+        with self.assertRaises(ValidationError):
+            gender.full_clean()
+
+    def test_when_name_is_more_than_max_length_should_raise_error(self):
+        gender = Gender(name='Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+                          'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, '
+                          'when an unknown printer took a galley of type and scrambled it to make a type '
+                          'specimen book. It has survived not only five centuries, but also the leap into '
+                          'electronic typesetting, remaining essentially unchanged. It was popularised in '
+                          'the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, '
+                          'and more recently with desktop publishing software like Aldus PageMaker '
+                          'including versions of Lorem Ipsum.')
+        with self.assertRaises(ValidationError):
+            gender.full_clean()
+
+#GENDER TESTS
+
 #JOBTYPE TESTS
 
 class JobTypeTest(TestCase):
