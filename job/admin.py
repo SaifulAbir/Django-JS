a1ceb774baed387d3dd4fabf369a6dd8f491fd5e
saiful <saiful@ishraak.com>
2020-02-18 12:21:09 +0600

    search and list for job app in admin done

--- job/admin.py
+++ job/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from job.models import Company,JobType,Experience,Qualification,Gender,Industry,Job
+from job.models import Company, JobType, Experience, Qualification, Gender, Industry, Job
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'industry', 'employment_status', 'job_location', 'experience', 'qualification', 'gender', 'company_name',
+                    'division', 'district']
+    search_fields = ['title__icontains', 'industry__name__icontains', 'employment_status__name__icontains', 'job_location__icontains',
+                     'experience__name__icontains', 'qualification__name__icontains', 'gender__name__icontains',
+                     'company_name__name__icontains', 'division__name__icontains', 'district__name__icontains', 'zipcode__iexact']
+
 
 admin.site.register(Company)
 admin.site.register(JobType)
@@ -9,4 +18,4 @@ admin.site.register(Experience)
 admin.site.register(Qualification)
 admin.site.register(Gender)
 admin.site.register(Industry)
-admin.site.register(Job)
+admin.site.register(Job, JobAdmin)
