c26104ec1a08162ddbde881421566f9feedabc5d
saiful <saiful@ishraak.com>
2020-02-19 17:20:02 +0600

    sign up user interface done

--- pro/urls.py
+++ pro/urls.py
@@ -4,4 +4,6 @@ from pro.api import *
 urlpatterns = [
     path('profile_create/', TemplateView.as_view(template_name='profile.html')),
     path('create/', profile_create),
+    path('register/', TemplateView.as_view(template_name='register.html')),
+    path('terms-and-condition/', TemplateView.as_view(template_name='terms-and-condition.html')),
 ]
