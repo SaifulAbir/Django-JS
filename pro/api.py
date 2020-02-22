c200ea7d4177a3bdae388dbdff0aee01bd96790a
Rashed <rashed@ishraak.com>
2020-02-22 17:36:05 +0600

    signup api done

--- pro/api.py
+++ pro/api.py
@@ -35,6 +35,17 @@ def profile_create_with_user_create(request):
                 }
             }
         }
+    elif 'phone' not in profile_data:
+        data = {
+            'status': 'failed',
+            'code': 500,
+            "message": 'Mobile number cannot be empty',
+            "result": {
+                "user": {
+
+                }
+            }
+        }
     elif User.objects.filter(email=profile_data['email']).count()>0 :
         data = {
             'status': 'failed',
@@ -50,6 +61,11 @@ def profile_create_with_user_create(request):
         hash_password = make_password(profile_data['password'])
         user = User(email=profile_data['email'], password=hash_password, username=profile_data['email'])
         if user.save() :
+            if profile_data['terms_and_condition_status'] == 'on':
+                profile_data['terms_and_condition_status']=1
+            else:
+                profile_data['terms_and_condition_status'] = 0
+
             profile_obj = Professional(**profile_data)
             profile_obj.save()
             data = {
