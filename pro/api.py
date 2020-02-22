fa495e6c027dbd33978ed9cdbe98041a0bc30f36
saiful <saiful@ishraak.com>
2020-02-22 17:13:52 +0600

    login api done

--- pro/api.py
+++ pro/api.py
@@ -1,9 +1,15 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from rest_framework.decorators import api_view
-from rest_framework.status import HTTP_200_OK
 from rest_framework.utils import json
 from rest_framework.response import Response
 from pro.models import Professional
+from resources.strings_pro import *
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
 
 
 @api_view(["POST"])
@@ -12,4 +18,16 @@ def profile_create(request):
     profile_data['password'] = make_password(profile_data['password'])
     profile_obj = Professional(**profile_data)
     profile_obj.save()
+    return Response(HTTP_200_OK)
+
+@api_view(["POST"])
+def login(request):
+    login_data = json.loads(request.body)
+    if login_data["username"] is None or login_data["password"] is None:
+        return Response({'error': LOGIN_CREDENTIAL_BLANK_ERROR},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=login_data["username"], password=login_data["password"])
+    if not user:
+        return Response({'error': LOGIN_CREDENTIAL_ERROR_MSG},
+                        status=HTTP_404_NOT_FOUND)
     return Response(HTTP_200_OK)
--- pro/urls.py
+++ pro/urls.py
@@ -4,6 +4,7 @@ from pro.api import *
 urlpatterns = [
     path('profile_create/', TemplateView.as_view(template_name='profile.html')),
     path('create/', profile_create),
+    path('login/', login),
     path('register/', TemplateView.as_view(template_name='register.html')),
     path('terms-and-condition/', TemplateView.as_view(template_name='terms-and-condition.html')),
 ]
