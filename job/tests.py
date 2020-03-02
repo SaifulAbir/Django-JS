51f371166dc4a1edfbc5fb1d065c2fccb104c45b
saiful <saiful@ishraak.com>
2020-03-02 12:44:53 +0600

    job model modified with currency and vacancy

--- job/tests.py
+++ job/tests.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from job.models import Job,Company,JobType,Qualification,Gender,Experience,Industry
+from job.models import Job, Company, JobType, Qualification, Gender, Experience, Industry, Currency
 from location.models import Division, District
 
 
@@ -84,6 +84,41 @@ class IndustryTest(TestCase):
             industry.full_clean()
 
 #INDUSTRY TESTS
+
+#Currency TESTS
+
+class CurrencyTest(TestCase):
+
+    def test_when_everything_required_is_given_should_pass(self):
+        currency = Currency(name='BDT')
+        try:
+            currency.full_clean()
+        except:
+            self.fail()
+
+    def test_when_name_is_null_should_raise_error(self):
+        currency = Currency()
+        with self.assertRaises(ValidationError):
+            currency.full_clean()
+
+    def test_when_name_is_blank_should_raise_error(self):
+        currency = Currency(name='')
+        with self.assertRaises(ValidationError):
+            currency.full_clean()
+
+    def test_when_name_is_more_than_max_length_should_raise_error(self):
+        currency = Currency(name='Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+                                 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, '
+                                 'when an unknown printer took a galley of type and scrambled it to make a type '
+                                 'specimen book. It has survived not only five centuries, but also the leap into '
+                                 'electronic typesetting, remaining essentially unchanged. It was popularised in '
+                                 'the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, '
+                                 'and more recently with desktop publishing software like Aldus PageMaker '
+                                 'including versions of Lorem Ipsum.')
+        with self.assertRaises(ValidationError):
+            currency.full_clean()
+
+#Currency TESTS
 
 #QUALIFICATION TESTS
 
