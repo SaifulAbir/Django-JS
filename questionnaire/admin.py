d56636f5f843f933ed258ae0a2466d35be779d75
saiful <saiful@ishraak.com>
2020-03-07 15:14:46 +0600

    app added to admin

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from questionnaire.models import Questionnaire, QuestionnaireDetail
+from django_admin_listfilter_dropdown.filters import DropdownFilter, RelatedDropdownFilter
+
+class QuestionnaireDetailAdmin(admin.ModelAdmin):
+    list_display = ['questionnaire_id','question_id']
+
+class QuestionnaireAdmin(admin.ModelAdmin):
+    list_display = ['name','subject','topic','sub_topic','remarks']
+    list_filter = (('name',DropdownFilter),('subject',RelatedDropdownFilter),('topic',RelatedDropdownFilter),
+                   ('sub_topic',RelatedDropdownFilter))
+
+admin.site.register(Questionnaire, QuestionnaireAdmin)
+admin.site.register(QuestionnaireDetail, QuestionnaireDetailAdmin)
