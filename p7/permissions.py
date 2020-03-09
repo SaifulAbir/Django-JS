ce36b9071f7518a32e155ecd90e596e06d196bb7
saiful <saiful@ishraak.com>
2020-03-09 11:39:24 +0600

    api key authentication issues solved

--- p7/permissions.py
+++ p7/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.permissions import BasePermission
 
-class CustomForbidden(APIException):
+class ApiKeyForbidden(APIException):
     status_code = status.HTTP_403_FORBIDDEN
     default_detail = "App token not found"
 
@@ -15,6 +15,6 @@ class IsAppAuthenticated(BasePermission):
             app_token = None
         permission = bool(app_token == '96d56aceeb9049debeab628ac760aa11')
         if not permission:
-            raise CustomForbidden
+            raise ApiKeyForbidden
         else:
             return True
