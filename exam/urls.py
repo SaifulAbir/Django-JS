e97b230e0adadfe6a1b907bd8d7d22ccdb8774a8
Rashed <rashed@ishraak.com>
2020-03-11 15:35:01 +0600

    Exam instruction api done

--- exam/urls.py
+++ exam/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from exam.api import *
 
 urlpatterns = [
-    path('exam-info/<int:exam_id>', exam_instruction),
+    path('exam-instruction/<int:exam_id>', exam_instruction),
     path('enrolled-examlist/<int:user_id>', enrolled_examlist),
     path('featured-examlist/<int:user_id>', featured_examlist),
     path('recent-examlist/<int:user_id>', recent_examlist)
