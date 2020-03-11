754c5e006125f20fd201fb112e27deb2bbaf72de
shoab <s201514034>
2020-03-11 16:10:26 +0600

    exam css

---
+++ static/js/exam.js
@@ -0,0 +1,24 @@
+$('#search-questionnaire').on('click', function () {
+    $.ajax({
+        url: '/exams/exam/search_questionnaire/',
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+            $('#search-questionnaire-modal .modal-content').html(data.form);
+            $("#search-questionnaire-modal").modal('show');
+        }
+    });
+});
+
+function showQuestionnaire() {
+    $("#template-list").addClass('d-none');
+    $('#questionnaire-list').removeClass('d-none');
+}
+function showTemplate() {
+    $("#questionnaire-list").addClass('d-none');
+    $('#template-list').removeClass('d-none');
+}
+
+$('#questionnaire-table ').on('click','.removebutton',function () {
+    $(this).parent().parent().remove()
+});
