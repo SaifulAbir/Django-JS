ad44927b44d6b14dfb89623236c756e218c4a3cb
ishraak <123456/>
2020-03-22 17:58:33 +0600

    comment spelling problem fixed and name change to client name

--- testimonial/models.py
+++ testimonial/models.py
@@ -3,8 +3,8 @@ from resources import strings_testimonial
 
 
 class Testimonial(models.Model):
-    name = models.CharField(max_length=255,unique=True)
-    coment = models.TextField()
+    client_name = models.CharField(max_length=255,unique=True)
+    comment = models.TextField()
     profile_picture = models.ImageField(upload_to='testimonials/',verbose_name='Profile Picture',blank=True)
 
     class Meta:
