86ad13549d033d7e787a73c35c6bbae81c01860d
saifulislam <saiful@ishraak.com>
2020-03-22 14:54:41 +0600

    database changed to sqlite

--- p7/settings_dev.py
+++ p7/settings_dev.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'p7.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # DATABASES = {
 #     'default': {
