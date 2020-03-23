8b0f122c2de8c4b2d4c2f4f56cd688db3157b091
Ubuntu <ubuntu@ip-172-31-27-172.ec2.internal>
2020-03-23 05:32:20 +0000

    user change on install.sh file

--- shell/install.sh
+++ shell/install.sh
@@ -7,8 +7,8 @@
 #
 # cd /var/
 # sudo mkdir p7
-# sudo chown tanvir p7
-# git clone https://imrashed@bitbucket.org/p7_team/p7_repo.git p7
+# sudo chown ubuntu p7
+# git clone https://mahadi_ish@bitbucket.org/p7_team/p7_repo.git p7
 ############################################################
 
 sudo apt update
@@ -16,7 +16,7 @@ sudo apt install python3 python python-minimal python3-pip python3-dev libmysqlc
 pip3 install virtualenv
 cd /var/
 sudo mkdir p7_static
-sudo chown tanvir p7_static
+sudo chown ubuntu p7_static
 git checkout master
 cd p7
 virtualenv -p python3 venv
