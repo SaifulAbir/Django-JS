2a210f0331eee64f7f981525867b03eb881a04a3
Masum Reza <masum@ishraak.com>
2020-03-28 12:51:27 +0600

    CareerAdvice model tests problem solved

--- career_advice/tests.py
+++ career_advice/tests.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from career_advice.models import Career_Advice
+from career_advice.models import CareerAdvice
 from django.test import TestCase
 
 # Create your tests here.
@@ -7,9 +7,9 @@ from django.test import TestCase
 #CAREER_ADVICE_TEST#
 
 
-class Career_AdviceTest(TestCase):
+class CareerAdviceTest(TestCase):
     def test_when_everything_is_given_should_pass(self):
-        career_advice = Career_Advice(title='Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        career_advice = CareerAdvice(title='Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
                                       description='Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
                              'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, '
                              'when an unknown printer took a galley of type and scrambled it to make a type '
@@ -31,17 +31,17 @@ class Career_AdviceTest(TestCase):
             self.fail()
 
     def test_when_title_is_null_should_raise_error(self):
-        career_advice = Career_Advice()
+        career_advice = CareerAdvice()
         with self.assertRaises(ValidationError):
             career_advice.full_clean()
 
     def test_when_title_is_blank_should_raise_error(self):
-        career_advice = Career_Advice(title=' ')
+        career_advice = CareerAdvice(title=' ')
         with self.assertRaises(ValidationError):
             career_advice.full_clean()
 
     def test_when_title_is_more_than_max_length_should_raise_error(self):
-        career_advice = Career_Advice(title='Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+        career_advice = CareerAdvice(title='Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
                              'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, '
                              'when an unknown printer took a galley of type and scrambled it to make a type '
                              'specimen book. It has survived not only five centuries, but also the leap into '
@@ -53,27 +53,27 @@ class Career_AdviceTest(TestCase):
             career_advice.full_clean()
 
     def test_when_description_is_null_should_raise_error(self):
-        career_advice = Career_Advice()
+        career_advice = CareerAdvice()
         with self.assertRaises(ValidationError):
             career_advice.full_clean()
 
     def test_when_description_is_blank_should_raise_error(self):
-        career_advice = Career_Advice(description=' ')
+        career_advice = CareerAdvice(description=' ')
         with self.assertRaises(ValidationError):
             career_advice.full_clean()
 
     def test_when_author_is_null_should_raise_error(self):
-        career_advice = Career_Advice()
+        career_advice = CareerAdvice()
         with self.assertRaises(ValidationError):
             career_advice.full_clean()
 
     def test_when_author_is_blank_should_raise_error(self):
-        career_advice = Career_Advice(author=' ')
+        career_advice = CareerAdvice(author=' ')
         with self.assertRaises(ValidationError):
             career_advice.full_clean()
 
     def test_when_author_is_more_than_max_length_should_raise_error(self):
-        career_advice = Career_Advice(author='Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+        career_advice = CareerAdvice(author='Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
                              'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, '
                              'when an unknown printer took a galley of type and scrambled it to make a type '
                              'specimen book. It has survived not only five centuries, but also the leap into '
