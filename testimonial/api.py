cae0c7e225d7d503e9580615eb51b88410e9eda9
shoab <s201514034>
2020-03-30 18:08:32 +0600

    testimonial api modified

--- testimonial/api.py
+++ testimonial/api.py
@@ -1,3 +1,7 @@
+import json
+
+from django.http import HttpResponse
+
 from .models import Testimonial
 from .serializers import TestimonialSerializer
 # from .serializers import DistrictSerializer
@@ -5,6 +9,16 @@ from rest_framework.response import Response
 from rest_framework import generics
 
 
-class TestimonialList(generics.ListAPIView):
+def testimonial_list(request):
     queryset = Testimonial.objects.all()[:5]
-    serializer_class = TestimonialSerializer
+    for testimonials in queryset:
+        if not testimonials.profile_picture:
+            testimonials.profile_picture = "a"
+
+    data=[{
+        'client_name': str(testimonial.client_name),
+        'comment' : str(testimonial.comment),
+        'profile_pic': str(testimonial.profile_picture),
+    } for testimonial in queryset
+    ]
+    return HttpResponse(json.dumps(data), content_type='application/json')
--- testimonial/urls.py
+++ testimonial/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from testimonial.api import *
 
 urlpatterns = [
-    path('testimonial_show/',TestimonialList.as_view())
+    path('testimonial_show/',testimonial_list)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
