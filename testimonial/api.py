fb3476f8e11d37dd9028a6083365da925b6822f7
shoab <s201514034>
2020-03-31 12:14:46 +0600

    testimonial front end done

--- testimonial/api.py
+++ testimonial/api.py
@@ -1,6 +1,7 @@
 import json
 
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
 
 from .models import Testimonial
 from .serializers import TestimonialSerializer
@@ -8,17 +9,17 @@ from .serializers import TestimonialSerializer
 from rest_framework.response import Response
 from rest_framework import generics
 
-
+@api_view(["GET"])
 def testimonial_list(request):
     queryset = Testimonial.objects.all()[:5]
     for testimonials in queryset:
         if not testimonials.profile_picture:
-            testimonials.profile_picture = "a"
+            testimonials.profile_picture = "testimonials/alternate.png"
 
     data=[{
         'client_name': str(testimonial.client_name),
         'comment' : str(testimonial.comment),
-        'profile_pic': str(testimonial.profile_picture),
+        'profile_picture': 'media/'+str(testimonial.profile_picture),
     } for testimonial in queryset
     ]
     return HttpResponse(json.dumps(data), content_type='application/json')
