3e03b1e1cc1e4000b158105a6988241b8b43171c
Anik_Ishraak <Alam@Ishraak.com>
2020-03-31 11:16:14 +0600

    Icon API implementation and test complete

--- settings/urls.py
+++ settings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 from .api import *
 urlpatterns = [
-    path('settings', SettingsList.as_view()),
+    path('settings/', SettingsList.as_view()),
 ]
