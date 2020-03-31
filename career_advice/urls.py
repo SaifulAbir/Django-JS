28c919415c1757e70b4f2a1052a5ab4748bc4485
Anik_Ishraak <Alam@Ishraak.com>
2020-03-31 10:14:39 +0600

    fix url

--- career_advice/urls.py
+++ career_advice/urls.py
@@ -6,5 +6,5 @@ from .test_api import CareerAdviceshow
 urlpatterns = [
     path('career_advise_show/', CareerAdviseShow.as_view()),
     # TEST_API_TEST
-    path('show_career_advice/', C),
+    path('show_career_advice/', CareerAdviceshow.as_view()),
 ]
