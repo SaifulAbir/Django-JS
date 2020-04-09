796b31ed9eea143c1d0cc50375fff167303645af
Masum Reza <masum@ishraak.com>
2020-04-09 09:23:15 +0600

    job details modified as app store and play store not required

--- settings/models.py
+++ settings/models.py
@@ -6,8 +6,8 @@ class Settings(models.Model):
     facebook_url = models.URLField(verbose_name='Facebook URL')
     linkedin_url = models.URLField(verbose_name='Linkedin URL', blank=True, null=True)
     twitter_url = models.URLField(verbose_name='Twitter URL', blank=True, null=True)
-    appstore_url = models.URLField(verbose_name='App Store URL')
-    playstore_url = models.URLField(verbose_name='Play Store URL')
+    appstore_url = models.URLField(verbose_name='App Store URL', blank=True, null=True)
+    playstore_url = models.URLField(verbose_name='Play Store URL', blank=True, null=True)
     logo_url = models.ImageField(upload_to='logo/', blank=True)
 
     class Meta:
