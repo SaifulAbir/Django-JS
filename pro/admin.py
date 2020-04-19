b12ca1302dc20501ee68f3031240ef6a3eb1a7f6
Masum Reza <masum@ishraak.com>
2020-04-19 18:09:57 +0600

    education show api creation done

--- pro/admin.py
+++ pro/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from pro.models import Professional
+from job.models import Qualification
+from pro.models import Professional, ProfessionalEducation, Institute, Major
 
 admin.site.register(Professional)
+admin.site.register(ProfessionalEducation)
+admin.site.register(Institute)
+admin.site.register(Major)
