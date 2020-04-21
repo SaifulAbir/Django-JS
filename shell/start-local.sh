db243b41078b887f60d763b4fddfb458be257052
Rashed <rashed@ishraak.com>
2020-04-21 11:23:31 +0600

    job type icon added into recent job list

--- shell/start-local.sh
+++ shell/start-local.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 source venv/bin/activate
 pip install -r requirements/req-dev.txt
+python manage.py makemigrations --merge
 python manage.py migrate
 python manage.py makemigrations
 python manage.py migrate
