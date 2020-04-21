41e93f68dae3f1131f16d85ee15b620eb3ac0a4f
Anik_Ishraak <Alam@Ishraak.com>
2020-04-21 06:28:31 +0600

    Pro About Done

--- pro/admin.py
+++ pro/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from pro.models import Professional
+from pro.models import Professional,Religion,Nationality
 
 admin.site.register(Professional)
+admin.site.register(Religion)
+admin.site.register(Nationality)
