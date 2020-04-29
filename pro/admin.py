490ac2b784d891a4651736b61a1cf0dc7790ee0a
Masum Reza <masum@ishraak.com>
2020-04-29 11:53:28 +0600

    professional model added in admin

--- pro/admin.py
+++ pro/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from pro.models import Professional
+from pro.models import Professional, ProfessionalEducation, ProfessionalSkill, WorkExperience, Portfolio
 
 admin.site.register(Professional)
+admin.site.register(ProfessionalEducation)
+admin.site.register(ProfessionalSkill)
+admin.site.register(WorkExperience)
+admin.site.register(Portfolio)
--- pro/urls.py
+++ pro/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     path('professional_membership_save/', professional_membership_save),
     path('professional_certification_save/', professional_certification_save),
     path('professional_reference_save/', professional_reference_save),
-    # path('professional_info/<str:pk>/', professional_info),
+    path('professional_info/<str:pk>/', professional_info),
     path('myprofile_info/<str:pk>/', TemplateView.as_view(template_name='myprofile.html'),name='myprofile'),
 ]
