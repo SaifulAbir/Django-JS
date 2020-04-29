84674f1fc217841443070bee98e80ae8673ce0ed
Masum Reza <masum@ishraak.com>
2020-04-29 11:32:35 +0600

    professional myprofile added

--- pro/urls.py
+++ pro/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     path('professional_certification_save/', professional_certification_save),
     path('professional_reference_save/', professional_reference_save),
     # path('professional_info/<str:pk>/', professional_info),
-    path('my_profile/<str:pk>/', TemplateView.as_view(template_name='professional_profile.html'),name='my_profile'),
+    path('myprofile_info/<str:pk>/', TemplateView.as_view(template_name='myprofile.html'),name='myprofile'),
 ]
