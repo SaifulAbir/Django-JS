17bc398e499f3131c0f8ea6881216aa2cd0743b2
Masum Reza <masum@ishraak.com>
2020-04-29 15:56:32 +0600

    professional profile show personal details

--- pro/admin.py
+++ pro/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from pro.models import Professional, ProfessionalEducation, ProfessionalSkill, WorkExperience, Portfolio, Institute, \
-    Major
+    Major, Nationality
 
 admin.site.register(Professional)
 admin.site.register(ProfessionalEducation)
@@ -11,3 +11,4 @@ admin.site.register(WorkExperience)
 admin.site.register(Portfolio)
 admin.site.register(Institute)
 admin.site.register(Major)
+admin.site.register(Nationality)
