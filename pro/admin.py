cb9e4210571f2cbcfb1de0918d522bbfc4f71953
Masum Reza <masum@ishraak.com>
2020-04-29 12:29:34 +0600

    professional profile show education background

--- pro/admin.py
+++ pro/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from pro.models import Professional, ProfessionalEducation, ProfessionalSkill, WorkExperience, Portfolio
+from pro.models import Professional, ProfessionalEducation, ProfessionalSkill, WorkExperience, Portfolio, Institute, \
+    Major
 
 admin.site.register(Professional)
 admin.site.register(ProfessionalEducation)
 admin.site.register(ProfessionalSkill)
 admin.site.register(WorkExperience)
 admin.site.register(Portfolio)
+admin.site.register(Institute)
+admin.site.register(Major)
