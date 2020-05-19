15faceb1beff7632ce8eab0e5601fb2659e5f32b
anik <Alam@Ishraak.com>
2020-05-19 08:56:33 +0600

    Ready Companies UI and fix issues

--- job/urls.py
+++ job/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('validation-test', TemplateView.as_view(template_name='company-create.html')),
     path('job-detail/<slug:slug>/', TemplateView.as_view(template_name='job-details.html')),
     path('jobs/', TemplateView.as_view(template_name='job-list.html'), name='jobs'),
-    path('update/<str:pk>/', TemplateView.as_view(template_name='update-job.html')),
+    path('company-sign/', TemplateView.as_view(template_name='company_sign_in.html'), name='company_sign'),
+    path('company-forgot/', TemplateView.as_view(template_name='company_forget_password.html'), name='company_forgot'),
+    path('company-reset/', TemplateView.as_view(template_name='company_reset_password.html'), name='company_reset'),
+    path('company-reset-success/', TemplateView.as_view(template_name='company-reset-password-successful.html'), name='reset_success'),
+    path('company-edit/', TemplateView.as_view(template_name='company_edit_profile.html'), name='company_edit'),
 ]
